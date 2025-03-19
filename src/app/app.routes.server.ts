727889881bc8456751b5976a0d6a8c3c3cd4fd6c
AhmedSabry444 <ahmedsabry010oggy@gmail.com>
2025-03-19 17:38:07 +0200

    four

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -8,4 +8,8 @@ export const serverRoutes: ServerRoute[] = [
     path: 'category/:categoryName',
     renderMode: RenderMode.Client,
   },
+  {
+    path: 'mealdetails/:mealId',
+    renderMode: RenderMode.Client,
+  },
 ];
